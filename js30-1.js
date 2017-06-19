d83000966acef055ffe4cb2e9adefa3e49bf53b0
martinz <martinzhangyx@gmail.com>
2017-06-18 23:15:30 -0700

    +2

--- js30-1.js
+++ js30-1.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     window.addEventListener('keydown',function(e){
         $('').addClass('.onkey');
         play();
-    
+        stop();
         $('').removeClass('.onkey');
     })
 })
