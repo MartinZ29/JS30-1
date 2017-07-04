04a5987b91eac567a8484badda7a15b0402f260e
martinz <martinzhangyx@gmail.com>
2017-07-03 23:13:54 -0700

    queryselect

--- js30-1.js
+++ js30-1.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     window.addEventListener('keydown',function(e){
-        const audio = ;
+        const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
+        console.log(audio);
         const key = 
 
         $('.key').addClass('.onkey');
-        audio.play();
-        stop();
+        // audio.play();
+        // stop();
         $('.key').removeClass('.onkey');
     })
 })
