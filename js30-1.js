4747fb6ebe8f365774a2ab3bb3dac86668078608
martinz <martinzhangyx@gmail.com>
2017-07-03 23:32:47 -0700

    stop

--- js30-1.js
+++ js30-1.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     window.addEventListener('keydown',function(e){
         const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
         console.log(audio);
+        if(!audio) return;
         const key = 
 
         $('.key').addClass('.onkey');
