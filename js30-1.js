8c3b135c82f53034af4b490cfb2d0dd3e5cf67d8
martinz <martinzhangyx@gmail.com>
2017-07-05 23:17:04 -0700

    key selector

--- js30-1.js
+++ js30-1.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     window.addEventListener('keydown',function(e){
         const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
+        const key = document.querySelector('.key[data-key="${e.keyCode}"]');
         console.log(audio);
         if (!audio) return;
         audio.play();
         audio.currentTime = 0;
-        const key = 
+    
 
         $('.key').addClass('.onkey');
         
