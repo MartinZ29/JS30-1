da76edb8a6047838f8b978601d81ebb9973463d6
martinz <martinzhangyx@gmail.com>
2017-07-06 23:44:05 -0700

    clear

--- js30-1.js
+++ js30-1.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     window.addEventListener('keydown',function(e){
         const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
         const key = document.querySelector('.key[data-key="${e.keyCode}"]');
-        console.log(audio);
+        
         if (!audio) return;
         audio.play();
         audio.currentTime = 0;
